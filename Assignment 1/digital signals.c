2662c8f4568d0a69b69cf021230e5ac69956d7a1
Smartie1242 <marten95b@hotmail.nl>
2020-02-04 16:34:51 +0100

    Email added

--- Assignment 1/digital signals.c
+++ Assignment 1/digital signals.c
@@ -1,6 +1,6 @@
 /*file:    digital signals.c							   */
 /*author:  Joel Kiers (email: j.k.kiers@student.rug.nl)*/
-/*partner: Marten Bosma (......@student.rug.nl)*/
+/*partner: Marten Bosma (email: m.bosma.21@student.rug.nl)*/
 /*date:    4 Feb, 2020								   */
 /*version: 1.0 										   */
 /*Description: This program transforms a signal into a level representation	   */
