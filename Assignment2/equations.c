f3133203419e7ed44efb6be7a3339129112b608c
Smartie1242 <marten95b@hotmail.nl>
2020-02-15 12:08:50 +0100

    Basics

---
+++ Assignment2/equations.c
@@ -0,0 +1,19 @@
+/*file:    signals.c							   */
+/*author:  Joel Kiers (email: j.k.kiers@student.rug.nl)*/
+/*partner: Marten Bosma (email: m.bosma.21@student.rug.nl)*/
+/*date:    4 Feb, 2020								   */
+/*version: 1.0 										   */
+/*Description: This program transforms a signal into a level representation	   */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include "scanner.h"
+
+
+
+int main(int argc, char*argv[]) {
+
+	return 0;
+}
