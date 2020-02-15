9e92e20cf9e0b2a31106c6c27dd83a797721ea2d
Smartie1242 <marten95b@hotmail.nl>
2020-02-15 14:47:08 +0100

    Check for disqualifying symbols

--- Assignment2/equations.c
+++ Assignment2/equations.c
@@ -8,3 +8,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "scanner.h"
+
+void evalSymbols() {
+ return;
+}
+
+void evalEquations(List li) {
+	int equals = 0;
+		while (li != NULL) {
+
+	    /*switch (li->tt) {
+	    case Number:
+	      printf("%d ", (li->t).number);
+	      break;
+	    case Identifier:
+	      printf("%s ", (li->t).identifier);
+	      break;
+	    case Symbol:
+				evalSymbols(li->t).symbol);
+				printf("%c ", (li->t).symbol);
+	      break;
+	    }*/
+
+	    li = li->next;
+		}
+
+		return;
+}
+
+void scanEquations() {
+	char *ar;
+  List li;
+  printf("give an equation: ");
+  ar = readInput();
+  while (ar[0] != '!') {
+    li = tokenList(ar);
+    printList(li);
+		evalEquations(li);
+    free(ar);
+    freeTokenList(li);
+		printf("\ngive an equation: ");
+    ar = readInput();
+  }
+  free(ar);
+  printf("good bye\n");
+}
+
+int main(int argc, char*argv[]) {
+	scanEquations();
+
+
+
+	return 0;
+}
