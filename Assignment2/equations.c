7b12be885852b67e534b00e43fa93383cb8b7f08
Smartie1242 <marten95b@hotmail.nl>
2020-02-15 13:17:35 +0100

    Stuff

---
+++ Assignment2/equations.c
@@ -0,0 +1,10 @@
+/*file:    equations.c							   */
+/*author:  Joel Kiers (email: j.k.kiers@student.rug.nl)*/
+/*partner: Marten Bosma (email: m.bosma.21@student.rug.nl)*/
+/*date:    15 Feb, 2020								   */
+/*version: 1.0 										   */
+/*Description: This program 	   */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "scanner.h"
